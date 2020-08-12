21c86c25d79b4d66822a32b6f211ad7fce931d64
Tom Stewart <TomStewart@operationss-air.home>
2020-08-12 20:14:02 +0100

    changed title to be more sophisticated

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,11 @@ function Header(){
   return(
     <div className="header">
       <center>
-      <h1> Run Boris, Run! </h1>
+      <h1 class="title"> Run Boris, Run! </h1>
       <div className="link-group">
         <a href="/" className="button" >Home</a>
         <a href="/controls" className="button" >Controls</a>
         <a href="/play" className="button" >Play</a>
-        {/* <a href="/about" className="button" >About Us</a> */}
       </div>
       </center>
       <br/>
--- src/components/home.js
+++ src/components/home.js
@@ -7,14 +7,13 @@ console.log(image)
 
 function Home(){
   return(
-    <div clas="background-container">
     <div className="home">
       <center>
       <div class="row">
         <div class="col-sm-12">
           <div class="opening">
-        <p> Boris Johnson is on his way to Sainsbury's to get some milk and nappies, but in order to avoid another #barnardcastle, he has to ensure he remains socially distanced.</p>
-        <p> If you get too close to other people walking down the street the InfectionRate will go up and it will be game over! </p>
+        <p> Boris Johnson is on his way to Sainsbury's to get some milk and nappies, but in order to avoid another lockdown, he has to ensure he remains socially distanced.</p>
+        <p> If you get too close to other people walking down the street the Infection Rate will go up and it will be game over! </p>
         <p> It's your job to navigate Boris safely around the streets of London, keeping 2 meters away from fellow pedestrians. </p>
           </div>
         <img src={image} id="borisGame"></img>
@@ -22,7 +21,6 @@ function Home(){
         </div>
       </center>
     </div>
-  </div>
   )
 }
 
