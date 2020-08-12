b35b3168fb1362c5c13daa7a29c1ad026ec19671
Tom Stewart <TomStewart@operationss-air.home>
2020-08-12 18:38:48 +0100

    making website more responsive

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import image from '../images/Game-screenshot.png';
+// import '../home.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 console.log(image)
 
@@ -7,12 +9,16 @@ function Home(){
   return(
     <div className="home">
       <center>
-      <div class="background-container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="opening">
         <p> Boris Johnson is on his way to Sainsbury's to get some milk and nappies, but in order to avoid another #barnardcastle, he has to ensure he remains socially distanced.</p>
         <p> If you get too close to other people walking down the street the InfectionRate will go up and it will be game over! </p>
         <p> It's your job to navigate Boris safely around the streets of London, keeping 2 meters away from fellow pedestrians. </p>
+          </div>
         <img src={image} id="borisGame"></img>
-      </div>
+        </div>
+        </div>
       </center>
     </div>
   )
